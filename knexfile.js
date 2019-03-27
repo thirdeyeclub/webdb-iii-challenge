8f080a8bf6e31972b2252af3176bd5763ae2902e
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-03-27 17:08:19 -0400

    migration 1

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,8 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './database/lambda.sqlite3'
+    },
+    UseNullAsDefault: true,
   },
 };
